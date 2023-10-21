6d772ae1e12ab63fa8520a3b058b22809a707c62
Davide Capacchione <davide.capacchione@gmail.com>
2023-10-21 20:19:57 +0200

    delete_old_results

--- api/functions/cronjobs/delete_results_cronjob.js
+++ api/functions/cronjobs/delete_results_cronjob.js
@@ -1,6 +1,12 @@
 const nodeCron = require("node-cron");
+const { axiosInstance } = require("../axios");
 
 const Cronex = '0 0 1 * *'
-nodeCron.schedule(Cronex, () => {
+nodeCron.schedule(Cronex, async () => {
+    try{
+      await axiosInstance('/node/delete_old_results')  
+    }catch(error){
+        console.log("cancellazione non riuscita")
+    }
     
 });
