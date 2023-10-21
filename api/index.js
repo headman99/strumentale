886e7ce744ed1eed6f68d681b51ff982ac533335
Davide Capacchione <davide.capacchione@gmail.com>
2023-10-21 20:20:36 +0200

    niente

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 const scraperRoute = require("./routes/scraper.js")
 const cors = require("cors");
 const crawler_cronjob = require("./functions/cronjobs/crawler_cronjob");
-//const delete_results_cronjob = require("./functions/cronjobs/delete_results_cronjob");
+const delete_results_cronjob = require("./functions/cronjobs/delete_results_cronjob");
 
 dotenv.config();
 
